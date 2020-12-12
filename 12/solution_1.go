92d394434d442642a72ea1c36a24d65df26f0a98
Anirudh H M <anihm136@gmail.com>
2020-12-12 16:39:51 +0530

    Day 12

---
+++ 12/solution_1.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func Move(command string, direction *rune, ns *int, ew *int) {
+	newDirection := command[0]
+	amount, _ := strconv.Atoi(command[1:])
+	directions := map[int]rune{0: 'N', 1: 'E', 2: 'S', 3: 'W'}    // Use these to find the new 
+	revDirections := map[rune]int{'N': 0, 'E': 1, 'S': 2, 'W': 3} // direction of the ship
+	switch newDirection {
+	case 'N':
+		*ns += amount
+	case 'S':
+		*ns -= amount
+	case 'E':
+		*ew += amount
+	case 'W':
+		*ew -= amount
+	case 'R':
+		curIndex := revDirections[*direction]
+		curIndex = (curIndex + (amount / 90)) % 4
+		*direction = directions[curIndex]
+	case 'L':
+		curIndex := revDirections[*direction]
+		curIndex = curIndex - (amount / 90)
+		if curIndex < 0 {
+			curIndex += 4
+		}
+		*direction = directions[curIndex]
+	case 'F':
+		switch *direction {
+		case 'N':
+			*ns += amount
+		case 'S':
+			*ns -= amount
+		case 'E':
+			*ew += amount
+		case 'W':
+			*ew -= amount
+		}
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	f, _ := os.Open("test.txt")
+
+	s := bufio.NewScanner(f)
+
+	direction := 'E'
+	ns := 0
+	ew := 0
+
+	for s.Scan() {
+		Move(s.Text(), &direction, &ns, &ew)
+	}
+
+	fmt.Println("Manhattan distance:", Abs(ns)+Abs(ew))
+}
---
+++ 12/solution_2.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// Rotate waypoint around ship by given amount
+func RotateWaypoint(x *int, y *int, amt int, direction byte) {
+	if amt == 180 {
+		*x = -*x
+		*y = -*y
+	} else if (direction == 'R' && amt == 90) || (direction == 'L' && amt == 270) { // x = y, y = -x
+		var temp int
+			temp = *y
+			*y = -*x
+			*x = temp
+	} else { // x = -y, y = x
+		var temp int
+			temp = *y
+			*y = *x
+			*x = -temp
+	}
+}
+
+func Move(command string, wp_x *int, wp_y *int, ns *int, ew *int) {
+	instruction := command[0]
+	amount, _ := strconv.Atoi(command[1:])
+	switch instruction {
+	case 'R','L':
+		RotateWaypoint(wp_x, wp_y, amount, instruction)
+	case 'F':
+		*ns += *wp_y * amount
+		*ew += *wp_x * amount
+	case 'N':
+		*wp_y += amount
+	case 'S':
+		*wp_y -= amount
+	case 'E':
+		*wp_x += amount
+	case 'W':
+		*wp_x -= amount
+	}
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	f, _ := os.Open("test.txt")
+
+	s := bufio.NewScanner(f)
+
+	wp_x := 10
+	wp_y := 1
+	ns := 0
+	ew := 0
+
+	for s.Scan() {
+		Move(s.Text(), &wp_x, &wp_y, &ns, &ew)
+	}
+
+	fmt.Println("Manhattan distance:", Abs(ns)+Abs(ew))
+}
